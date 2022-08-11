5c4b4d38c40a9c2bb140df901326be7d3bb3b145
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2022-08-11 12:04:00 +0100

    Added colour to state

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,38 +1,53 @@
+import { isLabelWithInternallyDisabledControl } from '@testing-library/user-event/dist/utils';
 import { useEffect, useRef, useState } from 'react';
+import Cell from '../Cell/Cell';
 import Row from '../Row/Row';
 import styles from './Board.module.css';
 
+export type node = {
+    colour: string,
+    letter: string
+}
+
 const Board = () => {
 
-    const [rows, setRows] = useState(Array(6).fill([]).map(() => Array<string>(5).fill('')))
-    const row = useRef(0)
+    const [rows, setRows] = useState<node[][]>(Array(6).fill([]).map(() => Array(5).fill({}).map(() => {
+        return {
+            colour: "", 
+            letter: ""
+        }
+    })))
+
+    const [row, setRow] = useState(0)
 
     useEffect(() => {
         document.addEventListener("keydown", keyHandler)
         return () => document.removeEventListener("keydown", keyHandler)
-    }, [])
+    }, [rows, row])
 
     const keyHandler = ({key}: KeyboardEvent) => {
+        console.log(key)
 
-        if (key === "Enter" && row.current < 5) {
-            row.current = row.current + 1
+        if (key === "Enter" && row < 5) {
+            setRow(prev => prev + 1)
         }
 
         if (key === "Backspace") {
             setRows(prev => {
-                const result = prev.map(word => word.slice())
-                let lastIndex = result[row.current].indexOf('')
-                lastIndex = lastIndex === -1 ? result[row.current].length : lastIndex
-                result[row.current][lastIndex - 1] = ''
+                const result: node[][] = prev.map(word => word.map(cell => {return {...cell}}))
+                let lastIndex = result[row].findIndex(cell => cell.letter === '')
+                lastIndex = lastIndex === -1 ? result[row].length : lastIndex
+                result[row][lastIndex - 1].letter = ''
                 return result
             })
         }
 
         key = key.toUpperCase()
-        if (key.match(`^[A-Z]$`)) {
+        if (key.match(`^[A-Z]$`) && rows[row].some(cell => cell.letter === '')) {
             setRows(prev => {
-                const result = prev.map(word => word.slice())
-                result[row.current][result[row.current].indexOf('')] = key
+                const result: node[][] = prev.map(word => word.map(cell => {return {...cell}}))
+                const currentRow = result[row]
+                currentRow[currentRow.findIndex(cell => cell.letter === '')].letter = key
                 return result
             })
         }
--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -1,14 +1,15 @@
+import { node } from '../Board/Board';
 import Cell from '../Cell/Cell';
 import styles from './Row.module.css'
 
 type Props = {
-    word: string[]
+    word: node[]
 }
 
 const Row = ({word}: Props) => (
     <div className={styles.row}>
         {
-            word.map((letter, index) => <Cell key={index} letter={letter}/>)
+            word.map((node, index) => <Cell key={index} letter={node.letter}/>)
         }
     </div>
 )
