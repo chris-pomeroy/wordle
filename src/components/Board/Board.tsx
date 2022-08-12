4dc5bb574dabf2450d610bc40df61690f15f0578
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2022-08-12 09:21:07 +0100

    Refactoring

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -19,58 +19,72 @@ const Board = () => {
     const [row, setRow] = useState(0)
 
     useEffect(() => {
-        document.addEventListener("keydown", keyHandler)
-        return () => document.removeEventListener("keydown", keyHandler)
+        document.addEventListener("keydown", keyboardEventHandler)
+        return () => document.removeEventListener("keydown", keyboardEventHandler)
     }, [rows, row])
 
     const ANSWER = "ALLOW"
 
     const clone = (nodes: node[][]) : node[][] => nodes.map(word => word.map(cell => {return {...cell}}))
-    
-    const keyHandler = ({key}: KeyboardEvent) => {
-        console.log(key)
 
-        if (key === "Enter" && row < 6) {
-            setRows(prev => {
-                const result = clone(prev)
-                const currentRow = result[row]
-                const answer = ANSWER.split('')
-                currentRow.forEach((node, index) => {
-                    const answerIndex = answer.indexOf(node.letter)
-                    if (node.letter === answer[index]) {
-                        answer[index] = ''
-                        node.colour = 'green'
-                    } else if (answerIndex !== -1) {
-                        answer[answerIndex] = ''
-                        node.colour = 'yellow'
-                    }
-                })
-                return result
-            })
-            setRow(prev => prev + 1)
+    const enterKeyHandler = () => {
+        if (row > 5 || rows[row].some(cell => cell.letter === '')) {
+            return
         }
 
-        if (key === "Backspace" && rows[row][0].letter !== '') {
-            setRows(prev => {
-                const result = clone(prev)
-                let lastIndex = result[row].findIndex(cell => cell.letter === '')
-                lastIndex = lastIndex === -1 ? result[row].length : lastIndex
-                result[row][lastIndex - 1].letter = ''
-                return result
+        setRows(prev => {
+            const answer = ANSWER.split('')
+            const result = clone(prev)
+            result[row].forEach((node, index) => {
+                const answerIndex = answer.indexOf(node.letter)
+                if (node.letter === answer[index]) {
+                    answer[index] = ''
+                    node.colour = 'green'
+                } else if (answerIndex !== -1) {
+                    answer[answerIndex] = ''
+                    node.colour = 'yellow'
+                }
             })
-        }
-
-        key = key.toUpperCase()
-        if (key.match(`^[A-Z]$`) && rows[row].some(cell => cell.letter === '')) {
-            setRows(prev => {
-                const result = clone(prev)
-                const currentRow = result[row]
-                currentRow[currentRow.findIndex(cell => cell.letter === '')].letter = key
-                return result
-            })
-        }
+            return result
+        })
+        setRow(prev => prev + 1)
     }
 
+    const backspaceKeyHandler = () => {
+        if (rows[row][0].letter === '') {
+            return
+        }
+
+        setRows(prev => {
+            const result = clone(prev)
+            let lastIndex = result[row].findIndex(cell => cell.letter === '')
+            lastIndex = lastIndex === -1 ? result[row].length : lastIndex
+            result[row][lastIndex - 1].letter = ''
+            return result
+        })
+    }
+
+    const keyHandler = (key: string) => {
+        key = key.toUpperCase()
+        if (!key.match(`^[A-Z]$`) || rows[row].every(cell => cell.letter !== '')) {
+            return
+        }
+
+        setRows(prev => {
+            const result = clone(prev)
+            const currentRow = result[row]
+            currentRow[currentRow.findIndex(cell => cell.letter === '')].letter = key
+            return result
+        })
+    }
+    
+    const keyboardEventHandler = ({key}: KeyboardEvent) => {
+        switch(key) {
+            case "Enter": enterKeyHandler(); break;
+            case "Backspace": backspaceKeyHandler(); break;
+            default: keyHandler(key)
+        }
+    }
 
     return (
         <div className={styles.board}>
