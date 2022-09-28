0da911873ddea8f5ecd27ccff9e6a45eaeeb8b76
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2022-09-28 15:44:02 +0100

    Larger buttons

--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -8,6 +8,9 @@ type Props = {
 const Key = ({letter, onClick, classes}: Props) => {
     classes = classes.map(className => styles[className])
     classes.push(styles.key)
+    if (letter === "↵" || letter === "⌫") {
+        classes.push(styles.large)
+    }
     return <div className={classes.join(" ")} onClick={onClick}>{letter}</div>
 }
 
