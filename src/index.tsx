1e0ab1ec55c740b071439a1947a2e2f4c7650e6e
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2023-04-13 08:33:00 +0100

    Add reset stats link

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,16 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (window.location.href.includes("reset-stats")) {
+  localStorage.clear()
+  window.location.href = '/wordle'
+} else {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
