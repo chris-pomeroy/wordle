8223a48603100b0fae7b4f56f033826c46002a46
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2023-04-19 12:56:47 +0100

    Added unit test for statistic

---
+++ src/__tests__/components/modal/Statistic.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react'
+import Statistic from '../../../components/modal/Statistic'
+
+describe('Statistic component', () => {
+    it('renders correctly with provided props', () => {
+        const value = 42
+        const message = 'Lorem ipsum dolor'
+        render(<Statistic value={value} message={message} />)
+
+        expect(screen.getByText(value.toString())).toBeInTheDocument()
+        const messageLines = screen.queryAllByText(/Lorem|ipsum|dolor/)
+        expect(messageLines).toHaveLength(3)
+        expect(messageLines[0]).toHaveTextContent('Lorem')
+        expect(messageLines[1]).toHaveTextContent('ipsum')
+        expect(messageLines[2]).toHaveTextContent('dolor')
+    })
+})
