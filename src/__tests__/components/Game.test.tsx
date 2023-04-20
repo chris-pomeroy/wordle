d988af833fb624b71c229568ddaf2398cc81804b
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2023-04-20 18:06:34 +0100

    Removed WIP tests

--- src/__tests__/components/Game.test.tsx
+++ src/__tests__/components/Game.test.tsx
@@ -8,35 +8,4 @@ describe('Game', () => {
         const gameComponent = getByText('Wordle')
         expect(gameComponent).toBeInTheDocument()
     })
-
-    test('handles key press correctly', () => {
-        const { container, getByText, getByTitle } = render(<Game />)
-        const board = container.querySelector(".board")
-
-        fireEvent.keyDown(board, {key: 'a', code: 'KeyA'})
-
-        // const gameComponent = getByTestId('game-component')
-        // fireEvent.keyDown(gameComponent, { key: 'a', code: 'KeyA' })
-        // fireEvent.keyUp(gameComponent, { key: 'a', code: 'KeyA' })
-        // const letterA = getByText('A')
-        // expect(letterA).toBeInTheDocument()
-        // fireEvent.keyDown(gameComponent, { key: 'Enter', code: 'Enter' })
-        // const cell = getByTestId('cell-0-0')
-        // expect(cell).toHaveClass('incorrect')
-    })
-
-    test('handles game over correctly', () => {
-        const { getByTestId, getByText } = render(<Game />)
-        const gameComponent = getByTestId('game-component')
-        fireEvent.keyDown(gameComponent, { key: 'a', code: 'KeyA' })
-        fireEvent.keyUp(gameComponent, { key: 'a', code: 'KeyA' })
-        fireEvent.keyDown(gameComponent, { key: 'Enter', code: 'Enter' })
-        fireEvent.keyDown(gameComponent, { key: 'Enter', code: 'Enter' })
-        fireEvent.keyDown(gameComponent, { key: 'Enter', code: 'Enter' })
-        fireEvent.keyDown(gameComponent, { key: 'Enter', code: 'Enter' })
-        fireEvent.keyDown(gameComponent, { key: 'Enter', code: 'Enter' })
-        fireEvent.keyDown(gameComponent, { key: 'Enter', code: 'Enter' })
-        const modal = getByTestId('modal-component')
-        expect(modal).toBeInTheDocument()
-    })
 })
